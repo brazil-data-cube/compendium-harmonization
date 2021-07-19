95470c4b377bccd0201b29adb3cf2c58c38f3cab
Phelps <phelpsmemo@gmail.com>
2021-07-19 19:47:59 -0300

    fix output error

--- cfactor/nbar.py
+++ cfactor/nbar.py
@@ -7,8 +7,8 @@
 #
 
 import os
-
 from typing import List
+
 from plumbum.cmd import docker
 
 
@@ -32,7 +32,6 @@ def lc8_generate_angles(input_dir: str, scene_ids: List[str]) -> List[str]:
         organizational structure will follow the data standards provided 
         by the USGS.
     """
-    
     processed_scenes = []
     for scene_id in scene_ids:
         (
@@ -42,12 +41,12 @@ def lc8_generate_angles(input_dir: str, scene_ids: List[str]) -> List[str]:
                 "l8angs:latest", scene_id
             ]
         )()
-        
-        processed_scenes.append(processed_scenes)
+
+        processed_scenes.append(scene_id)
     return processed_scenes
 
 
-def lc8_nbar(input_dir: str, output_dir: str, scene_ids: List[str]) -> List:
+def lc8_nbar(input_dir: str, output_dir: str, scene_ids: List[str]) -> List[str]:
     """ToDo: Add description
     
     Args:
@@ -55,23 +54,89 @@ def lc8_nbar(input_dir: str, output_dir: str, scene_ids: List[str]) -> List:
         processed are located.
                         
         output_dir (str): Directory where the results will be saved.
+
+        scene_ids (List[str]): List with the scene_ids that should be processed.
+        The scene_ids defined must be equivalent to the scene directory names in
+        the `input_dir`.
             
     Returns:
-        List: List with full path for each scene that had the angles generated.
+        List[str]: List with full path for each nbar scene generated.
     """
-    
-
     processed_scenes = []
     for scene_id in scene_ids:
         (
             docker[
                 "run", "--rm",
-                "-v", f"{os.path.join(input_dir, scene_id)}:/mnt/input-dir:ro", 
-                "-v", f"{output_dir}:/mnt/output-dir:rw", 
-                "nbar:fix", 
+                "-v", f"{os.path.join(input_dir, scene_id)}:/mnt/input-dir:ro",
+                "-v", f"{output_dir}:/mnt/output-dir:rw",
+                "nbar:fix",
                 scene_id
             ]
         )()
-        
-        processed_scenes.append(os.path.join(output_dir, processed_scenes))
+
+        processed_scenes.append(os.path.join(output_dir, scene_id))
+    return processed_scenes
+
+
+def s2_sen2cor_nbar(input_dir: str, output_dir: str, scene_ids: List[str]) -> List[str]:
+    """ToDo: Add description
+
+    Args:
+        input_dir (str): Directory where the directories of the scenes to be
+        processed are located.
+
+        output_dir (str): Directory where the results will be saved.
+
+        scene_ids (List[str]): List with the scene_ids that should be processed.
+        The scene_ids defined must be equivalent to the scene directory names in
+        the `input_dir`.
+
+    Returns:
+        List[str]: List with full path for each nbar (sen2cor) scene generated.
+    """
+    processed_scenes = []
+    for scene_id in scene_ids:
+        (
+            docker[
+                "run", "--rm",
+                "-v", f"{input_dir}:/mnt/input-dir:ro",
+                "-v", f"{output_dir}:/mnt/output-dir:rw",
+                "nbar:fix-2",
+                scene_id
+            ]
+        )()
+
+        processed_scenes.append(os.path.join(output_dir, scene_id))
+    return processed_scenes
+
+
+def s2_lasrc_nbar(input_dir: str, output_dir: str, scene_ids: List[str]) -> List[str]:
+    """ToDo: Add description
+
+    Args:
+        input_dir (str): Directory where the directories of the scenes to be
+        processed are located.
+
+        output_dir (str): Directory where the results will be saved.
+
+        scene_ids (List[str]): List with the scene_ids that should be processed.
+        The scene_ids defined must be equivalent to the scene directory names in
+        the `input_dir`.
+
+    Returns:
+        List[str]: List with full path for each nbar (LaSRC) scene generated.
+    """
+    processed_scenes = []
+    for scene_id in scene_ids:
+        (
+            docker[
+                "run", "--rm",
+                "-v", f"{input_dir}:/mnt/input-dir:ro",
+                "-v", f"{output_dir}:/mnt/output-dir:rw",
+                "nbar:fix-2",
+                scene_id[:-5]
+            ]
+        )()
+
+        processed_scenes.append(os.path.join(output_dir, scene_id))
     return processed_scenes
