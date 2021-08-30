da2ebc864db79727991e5e1b59a11338160cd16a
Phelps <phelpsmemo@gmail.com>
2021-08-30 07:52:07 -0300

    fix os module import

--- analysis/pipeline/validation/solids.py
+++ analysis/pipeline/validation/solids.py
@@ -6,6 +6,8 @@
 # under the terms of the MIT License; see LICENSE file for more details.
 #
 
+import os
+
 from dagster import Field, List, String, Nothing
 from dagster import solid, InputDefinition
 
