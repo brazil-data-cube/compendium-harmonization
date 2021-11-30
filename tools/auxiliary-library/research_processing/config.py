13fa07293de2b267db38354d19df76e889d26fa0
marujore <rennanmarujo@gmail.com>
2021-11-30 22:15:39 +0000

    fix-l8

--- tools/auxiliary-library/research_processing/config.py
+++ tools/auxiliary-library/research_processing/config.py
@@ -9,9 +9,9 @@
 
 class EnvironmentConfig:
     """Execution environment configurations."""
-    NBAR_IMAGE = "marujore/nbar@sha256:154d96558ee7131664eba78ad07f730765127393e6ea459a4c3bcb8b51a5c662"
+    NBAR_IMAGE = "marujore/nbar@sha256:18100383032d43eb7d083e6d00357bccb7ef1879faaf917dcb444e579668946d"
 
     LASRC_IMAGE = "marujore/lasrc@sha256:e656dd89cd07a67cae86ece6a28ca70bbc15affc9064efa00ed13ed4337f5434"
     SEN2COR_IMAGE = "marujore/sen2cor@sha256:a4c689a4dcc5a353533a3b1f78c351e59a01a6febacffa398068e013be35d24b"
 
-    LANDSAT8_ANGLES_IMAGE = "marujore/l8angs@sha256:940d6bcbd765acdb20a69ec140029d7e14bfaa8e668344fb674447b439cc36db"
+    LANDSAT8_ANGLES_IMAGE = "marujore/l8angs@sha256:6416006a777ab15d2ac98aac998f0c994068ca8b6febc2d625e8ca644e837559"
