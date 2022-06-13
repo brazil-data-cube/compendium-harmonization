19c7ad309b491ca1db78e5507cccbcd4e60bd325
marujore <rennanmarujo@gmail.com>
2022-06-13 08:57:46 -0300

    Update compendium Images

--- tools/auxiliary-library/research_processing/config.py
+++ tools/auxiliary-library/research_processing/config.py
@@ -11,7 +11,7 @@ class EnvironmentConfig:
     """Execution environment configurations."""
     NBAR_IMAGE = "marujore/sensor-harm@sha256:184959139eb6671a865c2223d3e488572a2c5a257af8805e337a5efbe15b7281"
 
-    LASRC_IMAGE = "marujore/lasrc@sha256:1b9d95a8a4956980a94cf06a75241e1a0257cd641cda8b9a87da68c83be594cd"
-    SEN2COR_IMAGE = "marujore/sen2cor:2.9.0@sha256:253aa3e220ba9cfe2521b76b0dd5a4ba437f980271ec9ba60b9d591207982b8f"
+    LASRC_IMAGE = "marujore/lasrc@sha256:e98b53614d12dfb1272a8045365439c9621a979feff27cb0d6f9a928e9f12c12"
+    SEN2COR_IMAGE = "marujore/sen2cor:2.9.0@sha256:1572353cdab0d73661f1d83f71cffe4e35906cd969cbc85ad2903111f57f9110"
 
     LANDSAT8_ANGLES_IMAGE = "marujore/landsat-angles@sha256:907666f17aaf236aeb4ddf4bf16ed4705c3ae42aa416c9b5879deb1c754c3a64"
